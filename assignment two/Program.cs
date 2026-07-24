fc66ca14e415d93bc5b2d275802374d2e1b589bb
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-24 23:29:00 +0300

    Q1Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print both title and upperTitle to show that title did not change.

--- assignment two/Program.cs
+++ assignment two/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print both title and upperTitle to show that title did not change.
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine(title);
+            //Console.WriteLine(upperTitle);
+            #endregion
+
+
+            #region Q2
+
+            #endregion
+            #region Q3
+
+            #endregion
+            #region Q4
+
+            #endregion
+            #region Q5
+
+            #endregion
+            #region Q6
+
+            #endregion
+            #region Q7
+
+            #endregion
+            #region Q8
+
+            #endregion
+            #region Q9
+
+            #endregion
+            #region Q10
+
+            #endregion
         }
     }
 }
