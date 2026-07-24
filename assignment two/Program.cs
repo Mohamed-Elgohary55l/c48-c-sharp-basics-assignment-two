68f2437b43c1a0f708828b0801316e2a0c829d8b
Mohamed-Elgohary55l <mohamedelgohary476@gmail.com>
2026-07-24 23:17:47 +0300

    Add project files.

---
+++ assignment two/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assignment_two
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
